bb3d89cdf0f2cf4d2766fede1c6178421c0c191b
Suhail2010 <95209676+Suhail2010@users.noreply.github.com>
2022-01-31 10:48:03 +0100

    Add project files.

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckTemp()
+        {
+           return View();
+        }
+        [HttpPost]
+        public IActionResult CheckTemp(string name, float temp)
+        {
+            _ = new DoctorModel
+            {
+                Name = name,
+                Temp = temp
+            };
+
+            ViewBag.Message = DoctorModel.Check(name, temp);
+            return View();
+        }
+    }
+}
---
+++ Models/DoctorModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DoctorModel
+    {
+        public static string Check(string name, float temp)
+        {
+            if (temp >= 36.0 && temp <= 37.2)
+            {
+                return name + " No fever detected.";
+            }
+            else if (temp >= 37.3 && temp <= 42.0)
+            {
+                return name + " Fever detected. See a doctor.";
+            }
+            else
+            {
+                return "     ";
+            }
+            
+        }
+        public string Name { get; set; }
+        public float Temp { get; set; }
+    }
+}
